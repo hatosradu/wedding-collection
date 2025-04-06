59208fad5f41661fee9de0b0854456fbf28839b2
hatosradu <hatosradu@gmail.com>
2025-04-06 22:33:43 +0300

    login view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { UploadComponent } from './features/upload/upload.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
---
+++ src/app/features/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  private router: Router = inject(Router);
+  public email: FormControl<string | null> = new FormControl(null);
+
+  continue() {
+    const user = this.email.getRawValue()?.toString();
+    if (user) {
+      sessionStorage.setItem('user', user);
+      this.router.navigate(['/', 'upload']);
+    }
+  }
+}
